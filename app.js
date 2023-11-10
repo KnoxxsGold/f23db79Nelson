bd9a5051953639f9e2771e9867e88c0350f9c027
Brady Nelson <Cindersbane@gmail.com>
2023-11-09 22:13:41 -0600

    new repo

---
+++ app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var pastaRouter = require('./routes/pasta');
+var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/pasta', pastaRouter);
+app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
+
+app.get('/board', (req, res) => {
+  const { rows, cols } = req.query;
+  console.log(`rows: ${rows}, cols: ${cols}`);
+  res.render('board', { title: 'Board Display', rows, cols });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
